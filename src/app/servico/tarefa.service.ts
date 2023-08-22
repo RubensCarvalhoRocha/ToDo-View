9cbaac82d5d482925991cd551cd7ef455c439630
RubensCarvalhoRocha <rocharubens4@gmail.com>
2023-08-22 15:36:07 -0300

    Cadastrar e Selecionar funcionando+MelhoriaNaTela
    Co-authored-by: Lucas de Oliveira Santos <Lucasnnn@users.noreply.github.com>

--- src/app/servico/tarefa.service.ts
+++ src/app/servico/tarefa.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from "rxjs"
+import { Tarefa } from '../modelo/Tarefa';
 
 @Injectable({
   providedIn: 'root'
@@ -10,4 +12,15 @@ export class TarefaService {
   private url:string = 'http://localhost:8080'
 
   constructor(private http:HttpClient) { }
+
+  //Metodo para selecionar todas as tarefas
+  selecionar():Observable<Tarefa[]>{
+    return this.http.get<Tarefa[]>(this.url);
+  }
+
+  //Método Cadastrar tarefa
+  cadastrar(obj:Tarefa):Observable<Tarefa>{
+    return this.http.post<Tarefa>(this.url, obj);
+  }
+
 }
--- src/app/todolist/todolist.component.ts
+++ src/app/todolist/todolist.component.ts
@@ -1,10 +1,40 @@
 import { Component } from '@angular/core';
+import { Tarefa } from '../modelo/Tarefa';
+import { TarefaService } from '../servico/tarefa.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-todolist',
   templateUrl: './todolist.component.html',
-  styleUrls: ['./todolist.component.css']
+  styleUrls: ['./todolist.component.css'],
 })
 export class TodolistComponent {
+  headers = ['Tarefa', 'Prioridade', 'Realizado', 'Delete'];
 
+  //Objeto do tipo Tarefa
+  tarefa = new Tarefa();
+
+  //Jason de Tarefas
+  tarefas: Tarefa[] = [];
+
+  //Construtor
+  constructor(private servico: TarefaService) {}
+
+  //Metodo  Selecionar Tarefas
+  selecionar(): void {
+    this.servico.selecionar().subscribe((retorno) => (this.tarefas = retorno));
+  }
+
+  //Método Cadastrar tarefa
+  cadastrar(): void {
+    this.servico.cadastrar(this.tarefa).subscribe((retorno) => {
+      this.tarefas.push(retorno);
+    });
+    console.log(this.tarefas);
+  }
+
+  //Metodo de Inicialização
+  ngOnInit() {
+    this.selecionar();
+  }
 }
