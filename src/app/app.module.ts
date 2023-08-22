9a88b96c4853c75ec513ae564995504a12008269
RubensCarvalhoRocha <rocharubens4@gmail.com>
2023-08-22 11:31:34 -0300

    implementando Modelo e Service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { TodolistComponent } from './todolist/todolist.component';
 import { FormsModule } from '@angular/forms';
-
+import { HttpClientModule } from '@angular/common/http';
 @NgModule({
   declarations: [
     AppComponent,
@@ -12,7 +12,8 @@ import { FormsModule } from '@angular/forms';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/modelo/Tarefa.ts
@@ -0,0 +1,8 @@
+export class Tarefa{
+
+  id:number = 0;
+  descricao:string = "";
+  realizado: boolean = false;
+  prioridade: number = 0;
+
+}
